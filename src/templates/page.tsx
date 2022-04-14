83fe683eda3b82f801f5ad5e11fe3fb7cbfc2a85
João Fonseca <joaofvfonseca@gmail.com>
2022-04-14 01:14:52 +0100

    Fix page-website title separator

--- src/templates/page.tsx
+++ src/templates/page.tsx
@@ -11,7 +11,7 @@ export default function PageTemplate({ data }) {
   return (
     <>
       <Helmet
-        title={`${title} | ${
+        title={`${title} - ${
           config.title
         }`}
       />
